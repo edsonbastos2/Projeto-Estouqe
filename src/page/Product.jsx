f56063ad9017b15e39244d538a26f67867d2cc6a
Edson Bastos <edsonbastos2@hotmail.com>
2021-05-16 23:48:44 -0300

    Formatado formulário de cadastro

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {Card, Form, Button} from 'react-bootstrap';
+import {Card, Form, Button, Container, Row, Col} from 'react-bootstrap';
 import firebase from '../firebaseConnection';
 import { toast } from 'react-toastify';
 
@@ -42,43 +42,52 @@ export default function Product(){
     }
 
     return(
-        <div className="container">
+        <Container>
             <Card className="mt-5">
                 <Card.Header>Cadastrar Produto</Card.Header>
                 <Card.Body>
                     <Form>
-                        <Form.Group controlId="formBasicNome">
-                            <Form.Label>Nome do Produto</Form.Label>
-                            <Form.Control type="text" placeholder="Nome produto" value={nome} onChange={e => setNome(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicModelo">
-                            <Form.Label>Modelo do Produto</Form.Label>
-                            <Form.Control type="text" placeholder="Modelo produto" value={modelo} onChange={e => setModelo(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicFabricante">
-                            <Form.Label>Fabricante do Produto</Form.Label>
-                            <Form.Control type="text" placeholder="Fabricante do Produto" value={fabricante} onChange={e => setFabricante(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicLote">
-                            <Form.Label>Lote do Produto</Form.Label>
-                            <Form.Control type="text" placeholder="Lote do Produto" value={lote} onChange={e => setLote(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicSetor">
-                            <Form.Label>Setor do Produto</Form.Label>
-                            <Form.Control type="text" placeholder="Setor do Produto" value={setor} onChange={e => setSetor(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicUsuario">
-                            <Form.Label>Usuário do Produto</Form.Label>
-                            <Form.Control type="text" placeholder="Usuário do Produto" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicData">
-                            <Form.Label>Data Saída</Form.Label>
-                            <Form.Control type="date" placeholder="Data Saída" value={data} onChange={e => setData(e.target.value)}/>
-                        </Form.Group>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formBasicNome">
+                                <Form.Label>Nome do Produto</Form.Label>
+                                <Form.Control type="text" placeholder="Nome produto" value={nome} onChange={e => setNome(e.target.value)}/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formBasicModelo">
+                                <Form.Label>Modelo do Produto</Form.Label>
+                                <Form.Control type="text" placeholder="Modelo produto" value={modelo} onChange={e => setModelo(e.target.value)}/>
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formBasicFabricante">
+                                <Form.Label>Fabricante do Produto</Form.Label>
+                                <Form.Control type="text" placeholder="Fabricante do Produto" value={fabricante} onChange={e => setFabricante(e.target.value)}/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formBasicLote">
+                                <Form.Label>Lote do Produto</Form.Label>
+                                <Form.Control type="text" placeholder="Lote do Produto" value={lote} onChange={e => setLote(e.target.value)}/>
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formBasicSetor">
+                                <Form.Label>Setor do Produto</Form.Label>
+                                <Form.Control type="text" placeholder="Setor do Produto" value={setor} onChange={e => setSetor(e.target.value)}/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formBasicUsuario">
+                                <Form.Label>Usuário do Produto</Form.Label>
+                                <Form.Control type="text" placeholder="Usuário do Produto" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formBasicData">
+                                <Form.Label>Data Saída</Form.Label>
+                                <Form.Control type="date" placeholder="Data Saída" value={data} onChange={e => setData(e.target.value)}/>
+                            </Form.Group>
+                        </Form.Row>
+
                         <Button variant="primary" onClick={handleAdd}>Cadastrar</Button>{' '}
                     </Form>
                 </Card.Body>
             </Card>
-        </div>
+        </Container>
     )
 }
